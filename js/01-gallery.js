63cef6c4e6ca1041892793ecfa9e595ab238f4b9
Vladislav Shevchuk <109815421+vladikshevchuk@users.noreply.github.com>
2022-11-27 19:41:15 +0100

    task-01 finished

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,56 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+// import * as basicLightbox from 'basiclightbox';
 
 console.log(galleryItems);
+
+const galleryRef = document.querySelector('.gallery');
+galleryRef.insertAdjacentHTML('beforeend', createGallery(galleryItems));
+galleryRef.addEventListener('click', getOriginalImg);
+
+function createGallery(galleryItems) {
+    const galleryUp = galleryItems.map(({ preview, original, description }) => {
+    return `
+    <div class="gallery__item">
+        <a class="gallery__link" href="${preview}">
+            <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+            />
+        </a>
+    </div>
+    `
+    }).join('');
+    
+    return galleryUp;
+}
+
+function getOriginalImg(evt) {
+    if (!evt.target.classList.contains('gallery__image')) {
+        return;
+    }
+
+    console.log(evt.target.dataset.source);
+
+    const instance = basicLightbox.create(`
+        <img src="${evt.target.dataset.source}" width="800" height="600">
+    `)
+
+    instance.show()
+
+    if (instance.show) {
+        document.addEventListener("keydown", event => {
+            event.code === 'Esc';
+            return instance.close();
+        });
+    }
+}
+
+
+
+
+
+
+
